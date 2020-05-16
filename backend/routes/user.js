6c4a63e3de526c43781aab9e30f0c6954ba31312
lakhan <lakhannad1999@gmail.com>
2020-05-16 16:28:46 +0530

    Changes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,20 +1,21 @@
-const router = require('express').Router();
-let Users = require('../models/user.model');
+const router = require("express").Router();
+const User = require("../models/user.model");
 
-router.route('/').get((req, res) => {
-    Users.find()
-        .then(user => res.join(user))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route("/").get((req, res) => {
+  User.find()
+    .then((user) => res.join(user))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
 
-    let newUser = new User({username});
+  let newUser = new User({ username });
 
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
