001dda8e7825e24a3fd41196cc97547a772f89e3
lakhan <lakhannad1999@gmail.com>
2020-05-27 11:33:59 +0530

    Static File Added

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../")));
 
 app.get("/", function (req, res) {
   res.render("save");
