af8ac6fa393f9327637226863b65b02126617ffe
PrernaSultania <prernasultania2000@gmail.com>
2020-05-29 17:22:43 +0530

    server.js

--- js/search.js
+++ js/search.js
@@ -14,36 +14,41 @@ const app = angular.module("homeApp", []);
       for(i=0; i < data.length ; i++)
       {
         let detail = {};
-        detail.id = data[i].payment.transactionId;
-        detail.name=[];
-        detail.fee = 0;
-        detail.paidbool=data[i].payment.paid;
-        if(detail.paidbool)
-        detail.type = data[i].payment.type;
-        else
-        detail.type = "--";
-        if(detail.paidbool == true)
-        detail.paid = "Paid";
-        else
-        detail.paid = "Failed";
+        detail.name = data[0];
+        detail.dor=data[3];
+        detail.crf =data[4];
+        detail.des=data[5];
+        detail.chg=data[1];
+        detail.sev=data[2];
+        detail.exp=data[6];
+        detail.req=data[7];
 
-        if(detail.paid=="Paid")
-        {
-          if(data[i].payment.dtxndate)
-          detail.date = data[i].payment.dtxndate;
-          else
-          detail.date = data[i].payment.online.transactionDate;
-        }
-        else
-        detail.date = data[i].payment.dtxndate;
+        // if(detail.paidbool)
+        // detail.type = data[i].payment.type;
+        // else
+        // detail.type = "--";
+        // if(detail.paidbool == true)
+        // detail.paid = "Paid";
+        // else
+        // detail.paid = "Failed";
 
-        for(j=0; j< data[i].eventIds.length ;j++ )
-        {
+        // if(detail.paid=="Paid")
+        // {
+        //   if(data[i].payment.dtxndate)
+        //   detail.date = data[i].payment.dtxndate;
+        //   else
+        //   detail.date = data[i].payment.online.transactionDate;
+        // }
+        // else
+        // detail.date = data[i].payment.dtxndate;
+
+        // for(j=0; j< data[i].eventIds.length ;j++ )
+        // {
           
-          detail.name.push(data[i].eventIds[j].name);
+        //   detail.name.push(data[i].eventIds[j].name);
           
-        }
-        detail.fee = data[i].payment.amount;
+        // }
+        // detail.fee = data[i].payment.amount;
       
         $scope.details.push(detail);
       }
